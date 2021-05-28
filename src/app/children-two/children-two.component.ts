587b36b93bf38951595fbef1ca54b7738c77f39d
Jagan P <jagan.rayaapati@gmail.com>
2021-05-28 09:02:58 +0530

    Added UI to request CRUD in mongodb

--- src/app/children-two/children-two.component.ts
+++ src/app/children-two/children-two.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-children-two',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildrenTwoComponent implements OnInit {
 
-  constructor() { }
+  private catList;
+  private addCatForm: FormGroup;
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.catList = this.httpClient.get("http://localhost:3000/cats");
+
+    this.addCatForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required])
+    })
+
+  }
+
+  addCat() {
+    console.log(this.addCatForm.value);
+    this.httpClient.post("http://localhost:3000/cats", this.addCatForm.value).subscribe((data)=>{
+      console.log(data);
+    });
   }
 
 }
